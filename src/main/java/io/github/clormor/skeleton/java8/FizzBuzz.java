c0c3352eca4fe4d2b6b013be276b1259934dc942
Chris Lormor <clormor@palantir.com>
2020-01-21 10:34:11 +0000

    declare FizzBuzz as functional

--- src/main/java/io/github/clormor/skeleton/java8/FizzBuzz.java
+++ src/main/java/io/github/clormor/skeleton/java8/FizzBuzz.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+@FunctionalInterface
 public interface FizzBuzz {
     List<String> fizzBuzz();
 }
